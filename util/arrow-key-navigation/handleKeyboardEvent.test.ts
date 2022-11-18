77e34c90d544ccdd0f28beb1da8eaa9e291360a2
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2022-11-18 16:38:24 +0100

    jacek 1

--- util/arrow-key-navigation/handleKeyboardEvent.test.ts
+++ util/arrow-key-navigation/handleKeyboardEvent.test.ts
@@ -1,8 +1,26 @@
+import { findNextTarget } from './findNextTarget'
 import { handleKeyboardEvent } from "./handleKeyboardEvent";
+
+jest.mock('./findNextTarget.ts')
 
 describe(`handleKeyboardEvent`, () => {
   it("does nothing if target not found", () => {
+    // given
     const keyboardEvent = new KeyboardEvent("keydown", { key: "ArrowLeft" });
-    handleKeyboardEvent(keyboardEvent);
+
+    const container = document.createElement('div')
+    const currentTarget = document.createElement('div')
+    container.appendChild(currentTarget)
+    container.addEventListener('keydown', handleKeyboardEvent)
+
+    // when
+    currentTarget.dispatchEvent(keyboardEvent)
+
+    // then
+    expect(findNextTarget).toHaveBeenCalledWith(
+      keyboardEvent.target,
+      'left',
+      keyboardEvent.currentTarget,
+    )
   });
 });
