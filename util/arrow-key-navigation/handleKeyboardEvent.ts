5e90c2e84910fa0cdb967a532f844fa2779d8dc4
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2022-11-21 16:58:09 +0100

    jacek 4

--- util/arrow-key-navigation/handleKeyboardEvent.ts
+++ util/arrow-key-navigation/handleKeyboardEvent.ts
@@ -7,7 +7,7 @@ export function handleKeyboardEvent(event: KeyboardEvent) {
     return
   }
 
-  findNextElementToFocus(event.target as HTMLElement, event.currentTarget as HTMLElement, direction);
+  findNextElementToFocus(event.target as HTMLElement, event.currentTarget as HTMLElement, direction)?.focus();
 }
 
 const keysToDirections: Record<string, Direction> = {
