84d4501895909e46a0c1b3242eae799e7707f2e4
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2022-11-28 07:39:15 +0100

    Use Babel instead of TS-Jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   resetMocks: true,
-};
+  transform: {
+    '\\.[jt]sx?$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-typescript',
+      ],
+    }],
+  },
+}
